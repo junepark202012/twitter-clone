129ee94b184bd3693d58ea97338168210dc393cc
June Park <junepark202012@gmail.com>
2022-10-04 16:46:56 +0900

    Set up dark mode and tailwind colorsˆ

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@ import TheHeader from '~/components/TheHeader.vue'
 </script>
 
 <template>
-  <div id="default-layout">
+  <div id="default-layout" class="grid grid-cols-12">
     <TheHeader />
     <slot />
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const myColors = {
+  mono: {
+    100: '#e7e9ea',
+    200: '#eff3f4',
+    400: '#e6e7e7',
+    600: '#536471',
+    800: '#1e2328',
+    900: '#0f1419',
+  },
+  primary: {
+    100: '#e8f5fe',
+    400: '#1d9bf0',
+  },
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   darkMode: 'class',
   theme: {
+    colors: {
+      black: myColors.mono['900'],
+      ...defaultTheme.colors,
+    },
     screens: {
       xs: '614px',
       sm: '1002px',
@@ -11,6 +32,11 @@ module.exports = {
       xl: '1280px',
     },
     extend: {
+      colors: {
+        ...myColors,
+        lightLogo: myColors.primary['100'],
+        darkLogo: myColors.primary['400'],
+      },
     },
   },
   plugins: [],
